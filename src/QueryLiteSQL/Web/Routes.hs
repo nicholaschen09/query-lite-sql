cca5c48f5cdb60dfa1ffff772a77dd266355a45f
nicholaschen09 <nicholas.chen243@gmail.com>
2025-05-09 16:46:11 -0400

    added routes

--- src/QueryLiteSQL/Web/Routes.hs
+++ src/QueryLiteSQL/Web/Routes.hs
@@ -6,7 +6,7 @@ module QueryLiteSQL.Web.Routes where
 import Web.Scotty.Trans
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Reader (ask)
-import Data.Aeson (Value, encode)
+import Data.Aeson (Value, encode, object, (.=))
 import Data.Text.Lazy (Text)
 import Data.Text.Lazy.Encoding (decodeUtf8)
 import Data.ByteString.Lazy (toStrict)
@@ -14,6 +14,7 @@ import Data.Time (getCurrentTime)
 
 import QueryLiteSQL.Types.Env (Env(..))
 import QueryLiteSQL.Parser.SQL (parseSQL)
+import QueryLiteSQL.Parser.Executor (executeQuery)
 import QueryLiteSQL.Database.Schema (saveQuery, getQueryHistory)
 
 routes :: ScottyT Text (ReaderT Env IO) ()
@@ -31,9 +32,11 @@ routes = do
         case parseSQL query of
             Left err -> json $ object ["error" .= err]
             Right sqlQuery -> do
-                result <- liftIO $ executeQuery env sqlQuery
-                liftIO $ saveQuery (dbConnection env) query (decodeUtf8 $ toStrict $ encode result)
-                json result
+                case executeQuery (jsonData env) sqlQuery of
+                    Left err -> json $ object ["error" .= err]
+                    Right result -> do
+                        liftIO $ saveQuery (dbConnection env) query (decodeUtf8 $ toStrict $ encode result)
+                        json result
 
     get "/api/history" $ do
         env <- ask
